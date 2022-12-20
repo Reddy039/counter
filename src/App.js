e65e0c3ffa4b17749edac8e538ad5caa113978f7
Reddy Rani <reddyrani039@gmail.com>
2022-12-20 11:32:31 +0530

    counter-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Counter from './Components/Counter'
+// import Step from "./Components/Step"
+
+function App(props) {
+ return( 
+  <div>
+    <Counter></Counter>
+    {/* <Step step={Step}></Step> */}
+  </div>
+ )
+}
+
+export default App;
---
+++ src/Components/Counter.js
@@ -0,0 +1,37 @@
+import React, {useState } from 'react'
+import './Counter.css'
+const Counter = () => {
+    const [num, setNum] = useState(0)
+    const [step,setStep]=useState(1)
+    const incHandler=(e)=>{
+        e.preventDefault()
+        setNum(num + (+step))
+    }
+    const decHandler=(e)=>{
+        e.preventDefault()
+        setNum(num-step)
+    }
+    const stepHandler=(e)=>{
+        e.preventDefault()
+        setStep(e.target.value)
+    }
+    return (
+        <>
+        <div>
+            <div className='boxy'>
+            <div classNmae='dis'>
+            <h1>Counter</h1>
+                <h2 className='text'> {num} </h2>
+            </div>
+            <div classNmae='box'>
+                <button type='submit' onClick={incHandler}> + </button>
+                <button type='submit' onClick={decHandler}> - </button>
+            </div>
+            <div className='step'><input type="number" onChange={stepHandler}></input></div>
+            </div>
+        </div>
+        </>
+
+    )
+}
+export default Counter
